7b071d231761bedb8c58e3cf9d5ce1b109a68f44
Abraam Demyan <abra.1020@gmail.com>
2024-12-04 19:06:53 -0700

    test

---
+++ churchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  churchApp
+//
+//  Created by Abraam Demyan on 11/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
